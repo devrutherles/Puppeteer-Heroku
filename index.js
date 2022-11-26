c7f93e0019c76fb95ec279886c5e142359130895
Brendo Rutherles <rutherles@gmail.com>
2022-11-25 23:06:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,31 +3,13 @@ const puppeteer = require('puppeteer')
    const path = require('path')
 
       const downloadPath = path.resolve('./download')
-
-class Envato {
- 
-
-
-
-  init = async () => {
-
-    try {
-    console.log("Opening the browser......");
-    this.browser = await puppeteer.launch({
-      headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      ignoreHTTPSErrors: true,
-      userDataDir: dir,
-    });
-  } catch (err) {
-    console.log("Could not create a browser instance => : ", err);
-  }     
-     
-
-
-
-
-
+      
+      app.get("/", async (req, res) => {
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
+  
+        
     const page = await this.browser.newPage();
     await page.goto(
       "https://elements.envato.com/pt-br/notepaper-hanging-on-clothesline-PHGVKY9"
@@ -61,6 +43,25 @@ await page.evaluate((btnSelector) => {
 
    
    }, 5000);
+        
+        
+
+  await browser.close();
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("starting....");
+});
+
+ 
+
+
+
+ 
+
+
+
+
 
 
     
@@ -77,27 +78,4 @@ await page.evaluate((btnSelector) => {
 
     
 
-    
-    // login
-    // await page.type('input[name="username"]', "amaroabril91@gmail.com")
-    //await page.type('input[name="password"]', "goleiro2014")
-    //await page.click('button[data-test-selector="sign-in-submit"]')
-    //await page.waitForNavigation({waitUntil: 'networkidle0'})
-    //console.log('signed in')
-  }
-
-  downloadPresentationTemplates = async () => {
-
-  }
-}
-
-const main = async () => {
-  const envato = new Envato(false)
-  await envato.init()
-
-
-
-
-}
-
-main().then()
+  
